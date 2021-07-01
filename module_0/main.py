1735a50713c68f1080ca51aae3c7dfbba9dd87d0
vabutuzov <butuzov@inbox.ru>
2021-07-01 15:14:36 +0300

    Updated module_0

--- module_0/main.py
+++ module_0/main.py
@@ -1,18 +1,35 @@
 import numpy as np
 
-count = 0  # счетчик попыток
-number = np.random.randint(1, 101)  # загадали число
-print("Загадано число от 1 до 100")
+def game_core_v3(number):
+    ''' На каждом шаге выбирается случайное число из заднного диапазона поиска.
+        Границы даипазона изменяются в зависимости в зависимости от того, больше проверяемое число или меньше нужного.
+        Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    low_limit = 1   # нижняя граница диапазона поиска
+    up_limit = 100  # верхняя граница диапазона поиска
+    predict = np.random.randint(low_limit, up_limit + 1 )
+    while number != predict:
+        count += 1
+        if number > predict:
+            low_limit = predict + 1
+            predict = np.random.randint(low_limit, up_limit + 1)
+        elif number < predict:
+            up_limit = predict - 1
+            predict = np.random.randint(low_limit, up_limit + 1)
+    return count # выход из цикла, если угадали
 
-while True:  # бесконечный цикл
-    predict = int(input())  # предполагаемое число
-    count += 1  # плюсуем попытку
-    if number == predict:
-        break  # выход из цикла, если угадали
-    elif number > predict:
-        print(f"Угадываемое число больше {predict} ")
-    elif number < predict:
-        print(f"Угадываемое число меньше {predict} ")
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=1000)
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return score
 
-print(f"Вы угадали число {number} за {count} попыток.")
+# запускаем
+score_game(game_core_v3)
+
 
